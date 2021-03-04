3df6acf4b9afeb94e2ee6adf989f933273be39e9
kaushalkr27 <kaushalkr27@gmail.com>
2021-03-05 02:06:03 +0530

    fixes

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,27 +1,27 @@
-var port = chrome.runtime.connect({name: "neoscale"});
+var port = chrome.runtime.connect({ name: "neoscale" });
 
-port.onMessage.addListener(function(msg) {
+port.onMessage.addListener(function (msg) {
     console.log("port active");
-  if (msg.message == "2"){
-    start_2();
-    console.log("recieved 22222");
-  }  
-  else if (msg.message == "3"){
-    console.log("recieved 33333");
-    start_3();
-  }
-  else if(msg.message == "4"){
-    console.log("recieved 44444");
-    start_4();
-  }
-  else if(msg.message == "5"){
-    console.log("recieved 55555");
-    start_5();
-  }
-  else if(msg.message == "6"){
-    console.log("recieved 66666");
-    start_6();
-  }
+    if (msg.message == "2") {
+        start_2();
+        console.log("recieved 22222");
+    }
+    else if (msg.message == "3") {
+        console.log("recieved 33333");
+        start_3();
+    }
+    else if (msg.message == "4") {
+        console.log("recieved 44444");
+        start_4();
+    }
+    else if (msg.message == "5") {
+        console.log("recieved 55555");
+        start_5();
+    }
+    else if (msg.message == "6") {
+        console.log("recieved 66666");
+        start_6();
+    }
 });
 
 chrome.runtime.onMessage.addListener(
@@ -33,15 +33,15 @@ chrome.runtime.onMessage.addListener(
     }
 );
 
-function sendMesssageToBakground(json){
-    port.postMessage(json, function(){
+function sendMesssageToBakground(json) {
+    port.postMessage(json, function () {
         console.log("Sent from content");
     });
 }
 
-function clickOnSearch(searchButton){
+function clickOnSearch(searchButton) {
     searchButton.click();
-    sendMesssageToBakground({"message":"completed 1"})
+    sendMesssageToBakground({ "message": "completed 1" })
 }
 
 function start_1() {
@@ -51,62 +51,62 @@ function start_1() {
     setTimeout(clickOnSearch, 3000, searchButton);
 }
 
-function start_2(){
+function start_2() {
     console.log("Started 2");
     console.log("22222222");
     var allSponsoredItems = document.querySelectorAll('[data-component-type="sp-sponsored-result"]');
     var item = allSponsoredItems[1].getElementsByTagName('a');
     console.log(item);
     item[0].click();
-    sendMesssageToBakground({"message":"completed 2"})
+    sendMesssageToBakground({ "message": "completed 2" })
 }
 
-function start_3(){
+function start_3() {
     var image = document.getElementsByClassName('a-dynamic-image');
     image[0].click();
-    sendMesssageToBakground({"message":"completed 3"})
+    sendMesssageToBakground({ "message": "completed 3" })
 }
 
-function start_4(){
+function start_4() {
     var images = document.getElementById('ivThumbs').getElementsByClassName('ivThumbImage');
     console.log("-----");
     console.log(images.length);
     console.log(images);
     console.log("-----");
-    var i= 1;
-    var interval = setInterval(function(){
+    var i = 1;
+    var interval = setInterval(function () {
         images[i].click();
-        if(i >= images.length - 1){
+        if (i >= images.length - 1) {
             var closeButton = document.getElementsByClassName('a-icon-close')[1];
             closeButton.click();
             clearInterval(interval);
-            sendMesssageToBakground({"message":"completed 4"})
+            sendMesssageToBakground({ "message": "completed 4" })
         }
         i = i + 1;
     }, 3000);
 }
 
-function start_5(){
+function start_5() {
     var element = document.getElementsByClassName('a-icon-star')[0];
-    element.addEventListener('mouseover', function() {
-        setTimeout(function(){
+    element.addEventListener('mouseover', function () {
+        setTimeout(function () {
             var oneStar = document.getElementsByClassName('1star')[1];
             oneStar.click();
-            sendMesssageToBakground({"message":"completed 5"})
+            sendMesssageToBakground({ "message": "completed 5" })
         }, 2000);
     });
 
     var event = new MouseEvent('mouseover', {
-    'view': window,
-    'bubbles': true,
-    'cancelable': true
+        'view': window,
+        'bubbles': true,
+        'cancelable': true
     });
 
     element.dispatchEvent(event);
 }
 
-function start_6(){
-    setTimeout(function(){
+function start_6() {
+    setTimeout(function () {
         console.log("SCROLL");
         document.getElementsByClassName('review')[2].scrollIntoView({
             behavior: 'smooth'
